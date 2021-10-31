4e4a71ec3a72f8570a3952f88880b3784a583807
Serg_Grish <balchyg@icloud.com>
2021-10-31 19:12:23 +0200

    api update

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -2,3 +2,44 @@ import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "f064d7186c8833896e84ab69904f756d";
+
+const fetchMovies = {
+  fetchTrendingMoviesByDay() {
+    return axios
+      .get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`)
+      .then((response) => response.data.results)
+      .catch((error) => error);
+  },
+
+  fetchSearchMovie(querySearch) {
+    return axios
+      .get(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&page=1&include_adult=false&query=${querySearch}`
+      )
+      .then((response) => response.data.results)
+      .catch((error) => error);
+  },
+
+  fetchMoviesById(movieId) {
+    return axios
+      .get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`)
+      .then((response) => response.data)
+      .catch((error) => error);
+  },
+
+  fetchMovieCast(movieId) {
+    return axios
+      .get(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`)
+      .then((response) => response.data)
+      .catch((error) => error);
+  },
+
+  fetchMovieReviews(movieId) {
+    return axios
+      .get(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`)
+      .then((response) => response.data)
+      .catch((error) => error);
+  },
+};
+
+export default fetchMovies;
